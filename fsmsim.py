d0e2c1446fc33e8d93499511fc74d8eca2594446
Young <mingywang@hotmail.com>
2015-03-26 18:59:31 +0800

    (non)determistic finite state machine

---
+++ fsmsim.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+# Finite State Machine Simulation
+# RE matches r"a+1+"
+
+edges = {(1, 'a') : 2,
+		 (2, 'a') : 2,
+		 (2, '1') : 3,
+		 (3, '1') : 3}
+
+accepting = [3]
+
+def fsmsim(string, current, edges, accepting):
+	if string == "":
+		return current in accepting
+	else:
+		letter = string[0]
+		if (current, letter) in edges:
+			return fsmsim(string[1:], edges[(current, letter)], edges, accepting)
+		else:
+			return False
+
+print fsmsim("aaa111", 1, edges, accepting)
+# >>> True
